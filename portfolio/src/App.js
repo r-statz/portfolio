c5d421cbe34c7e4c45cd804bc36158c54b77d84d
rstatz <robinstatz@gmail.com>
2018-10-03 20:32:56 -0600

    6 spinning carrots

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import carrot from './carrot.svg';
+import carrot2 from './carrot2.svg';
+import carrot3 from './carrot3.svg';
+import carrotBent from './carrotBent.svg';
+import carrotBent2 from './carrotBent2.svg';
+import carrotBent3 from './carrotBent3.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +12,28 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <div className="row">
+          <img src={carrotBent2} className="App-carrot2" alt="carrot" />
+          <img src={carrot} className="App-carrot3" alt="carrot" />
+          <img src={carrotBent3} className="App-carrot" alt="carrot" />
+        </div>
+        <div className="row">
+        <img src={carrotBent} className="App-carrot" alt="carrot" />
+        <img src={carrot3} className="App-carrot3" alt="carrot" />
+        <img src={carrot2} className="App-carrot2" alt="carrot" />
+      </div>
+
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            PORTFOLIOHOLIO
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
+          <div
+            className=""
+            href="https://"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
-          </a>
+            Plant  Eater
+          </div>
         </header>
       </div>
     );
